270a618410bb04850546b5ce677532b203b209f6
Peter Bateson <peterbateson@Peters-MacBook-Pro-2.local>
2016-12-30 10:22:34 +0800

    Initial commit

---
+++ app/src/main/java/com/peterbateson/drivingquiz/QuizActivity.java
@@ -0,0 +1,95 @@
+package com.peterbateson.drivingquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.TextView;
+
+
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private Button mCheatButton;
+    private TextView mQuestionTextView;
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_1, false),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, false),
+            new TrueFalse(R.string.question_4, false),
+            new TrueFalse(R.string.question_5, true),
+    };
+
+    public void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getmQuestion();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].ismTrueQuestion();
+
+        int messageResId = 0;
+
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    private int mCurrentIndex = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               checkAnswer(true);
+            }
+        });
+
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+            }
+        });
+
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = mCurrentIndex + 1;
+                updateQuestion();
+            }
+        });
+
+        mCheatButton = (Button)findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                  // Start CheatActivity
+             }
+        });
+
+        updateQuestion();
+    }
+
+}
---
+++ app/src/main/java/com/peterbateson/drivingquiz/TrueFalse.java
@@ -0,0 +1,28 @@
+package com.peterbateson.drivingquiz;
+
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getmQuestion() {
+        return mQuestion;
+    }
+
+    public void setmQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean ismTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}
